a71b2537c4236c314eea789d181bd196fb9f1ffc
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-04-25 19:20:06 +0600

    Pro 167 solved using Binary search approach

--- src/Pro167.java
+++ src/Pro167.java
@@ -3,18 +3,43 @@ public class Pro167 {
     public int[] twoSum(int[] numbers, int target) {
 
         // Two-Pointer Approach
-        int left = 0, right = numbers.length - 1;
-        while(left < right){
-            int sum = numbers[left] + numbers[right];
-            if(sum == target){
-                return new int[]{left + 1, right + 1};
-            } else if(sum < target){
-                left++;
-            } else {
-                right--;
+//        int left = 0, right = numbers.length - 1;
+//        while(left < right){
+//            int sum = numbers[left] + numbers[right];
+//            if(sum == target){
+//                return new int[]{left + 1, right + 1};
+//            } else if(sum < target){
+//                left++;
+//            } else {
+//                right--;
+//            }
+//        }
+//        return new int[]{-1, -1};
+
+
+        //Binary search approach
+        for (int i = 0; i < numbers.length; i++) {
+            int complement = target - numbers[i];
+
+            int left = i + 1;
+            int right = numbers.length - 1;
+
+            while (left <= right) {
+                int mid = (left + right) / 2;
+
+                if (numbers[mid] == complement) {
+                    return new int[]{i + 1, mid + 1};
+                } else if (numbers[mid] < complement) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
+
         return new int[]{-1, -1};
     }
+
+
 
 }
