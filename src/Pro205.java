a78a21df785d97a07cd7cd4d1d35dba576f7f8a4
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-05-05 14:46:52 +0600

    Pro205 prolved using HashMap and hashSet to eliminate hidden loop of .containsValue()

--- src/Pro205.java
+++ src/Pro205.java
@@ -1,11 +1,34 @@
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Pro205 {
 
     public boolean isIsomorphic(String s, String t) {
+//        if (s.length() != t.length()) return false;
+//
+//        HashMap<Character, Character> charMap = new HashMap<>();
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            char sChar = s.charAt(i);
+//            char tChar = t.charAt(i);
+//
+//            if (charMap.containsKey(sChar)) {
+//                if (charMap.get(sChar) != tChar) {
+//                    return false;
+//                }
+//            } else {
+//                if (charMap.containsValue(tChar)) {
+//                    return false;
+//                }
+//                charMap.put(sChar, tChar);
+//            }
+//        }
+//        return true;
+
         if (s.length() != t.length()) return false;
 
         HashMap<Character, Character> charMap = new HashMap<>();
+        HashSet<Character> claimedTChars = new HashSet<>();
 
         for (int i = 0; i < s.length(); i++) {
             char sChar = s.charAt(i);
@@ -15,14 +38,18 @@ public class Pro205 {
                 if (charMap.get(sChar) != tChar) {
                     return false;
                 }
-            } else {
-                if (charMap.containsValue(tChar)) {
+            }
+            else {
+                if (claimedTChars.contains(tChar)) {
                     return false;
                 }
+
                 charMap.put(sChar, tChar);
+                claimedTChars.add(tChar);
             }
         }
         return true;
+
     }
 
 }
