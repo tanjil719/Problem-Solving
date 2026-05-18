d7637fc99bfdc9b6621521a0166993370fade564
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-05-18 23:05:15 +0600

    Refactor twoSum method to use HashMap for improved efficiency

--- src/Pro1.java
+++ src/Pro1.java
@@ -4,25 +4,29 @@ public class Pro1 {
 
     public int[] twoSum(int[] nums, int target) {
 
-//        HashMap<Integer, Integer> elementCount = new HashMap<>();
-//        for (int i = 0; i < nums.length; i++) {
-//           int middle = target - nums[i];
-//            if (elementCount.containsKey(middle) && elementCount.get(middle) != i) {
-//                return new int[]{i, elementCount.get(middle)};
+//        for (int i = 1; i < nums.length; i++) {
+//            for (int j = 0; j < nums.length && j + i < nums.length; j++) {
+//                if (nums[j] == target - nums[j + i]) {
+//                    return new int[]{j, j + i};
+//                }
 //            }
-//            elementCount.put(nums[i], i);
 //        }
-//
-//        return new int[]{};
+//        return new int[]{0, 0};
 
-        for (int i = 1; i < nums.length; i++) {
-            for (int j = 0; j < nums.length && j + i < nums.length; j++) {
-                if (nums[j] == target - nums[j + i]) {
-                    return new int[]{j, j + i};
-                }
+        HashMap<Integer, Integer> elementCount = new HashMap<>();
+
+        for (int i = 0; i< nums.length; i++){
+            int element = target - nums[i];
+
+            if(elementCount.containsKey(element)){
+                return new int[]{elementCount.get(element), i};
+            } else {
+                elementCount.put(nums[i], i);
             }
         }
-        return new int[]{0, 0};
+
+        return new int[]{};
+
     }
 
 
