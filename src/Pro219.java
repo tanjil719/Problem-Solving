2f0731f0ff6e08010da7723892257caa2e10c925
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-05-24 15:35:39 +0600

    Refactor containsNearbyDuplicate method to use Sliding Window approach for improved performance

--- src/Pro219.java
+++ src/Pro219.java
@@ -1,18 +1,42 @@
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Pro219 {
 
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer, Integer> elementIndexMap = new HashMap<>();
+
+        //Using HashMap For solution
+
+//        HashMap<Integer, Integer> elementIndexMap = new HashMap<>();
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            int element = nums[i];
+//
+//            if (elementIndexMap.containsKey(element) && i - elementIndexMap.get(element) <= k) {
+//                return true;
+//            }
+//
+//            elementIndexMap.put(element, i);
+//        }
+//
+//        return false;
+
+
+        //Using Sliding Window For solution
+
+        Set<Integer> window = new HashSet<>();
 
         for (int i = 0; i < nums.length; i++) {
-            int element = nums[i];
-
-            if (elementIndexMap.containsKey(element) && i - elementIndexMap.get(element) <= k) {
+            if (window.contains(nums[i])) {
                 return true;
             }
 
-            elementIndexMap.put(element, i);
+            window.add(nums[i]);
+
+            if (window.size() > k) {
+                window.remove(nums[i - k]);
+            }
         }
 
         return false;
