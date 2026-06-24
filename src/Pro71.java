5162761fd297ff0e34d891f74ccc469e4d6a5d4b
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-06-25 00:06:13 +0600

    Solved pro 71 using ArrayDeque

--- src/Pro71.java
+++ src/Pro71.java
@@ -1,27 +1,55 @@
 import java.util.*;
 
 class Pro71 {
+//    public String simplifyPath(String path) {
+//
+//        String[] parts = path.split("/");
+//        Stack<String> stack = new Stack<>();
+//
+//        for (String part : parts) {
+//            if (part.equals("..")) {
+//                if (!stack.isEmpty()) {
+//                    stack.pop();
+//                }
+//            } else if (!part.equals("") && !part.equals(".")) {
+//                stack.push(part);
+//            }
+//        }
+//
+//        StringBuilder result = new StringBuilder();
+//        for (String dir : stack) {
+//            result.append("/").append(dir);
+//        }
+//
+//        return result.length() > 0 ? result.toString() : "/";
+//
+//    }
+
     public String simplifyPath(String path) {
 
-        String[] parts = path.split("/");
-        Stack<String> stack = new Stack<>();
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        String[] updatedPath = path.split("/");
 
-        for (String part : parts) {
-            if (part.equals("..")) {
-                if (!stack.isEmpty()) {
+        for (String dir : updatedPath){
+            if (dir.equals("..")){
+                if (!stack.isEmpty()){
                     stack.pop();
                 }
-            } else if (!part.equals("") && !part.equals(".")) {
-                stack.push(part);
+            } else if (!dir.equals("") && !dir.equals(".")){
+                stack.push(dir);
             }
         }
 
+
         StringBuilder result = new StringBuilder();
-        for (String dir : stack) {
+        List<String>  dirs = new ArrayList<>(stack);
+        Collections.reverse(dirs);
+        for (String dir : dirs) {
             result.append("/").append(dir);
         }
 
         return result.length() > 0 ? result.toString() : "/";
 
     }
+
 }
