ac1d1f7f0a905c5e1d5cd52da683defd08a25983
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-06-29 15:27:53 +0600

    Solved pro 150 using deque

--- src/Pro150.java
+++ src/Pro150.java
@@ -1,36 +1,63 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 public class Pro150 {
 
     public int evalRPN(String[] tokens) {
 
-        Stack<Integer> result = new Stack<>();
+//        Stack<Integer> result = new Stack<>();
+//
+//        for (int i = 0; i < tokens.length; i++) {
+//            String token = tokens[i];
+//
+//            if (token.equals("+")) {
+//                int a = result.pop();
+//                int b = result.pop();
+//                result.push(b + a);
+//            } else if (token.equals("-")) {
+//                int a = result.pop();
+//                int b = result.pop();
+//                result.push(b - a);
+//            } else if (token.equals("*")) {
+//                int a = result.pop();
+//                int b = result.pop();
+//                result.push(b * a);
+//            } else if (token.equals("/")) {
+//                int a = result.pop();
+//                int b = result.pop();
+//                result.push(b / a);
+//            } else {
+//                result.push(Integer.parseInt(token));
+//            }
+//        }
+//
+//        return result.pop();
 
-        for (int i = 0; i < tokens.length; i++) {
-            String token = tokens[i];
 
-            if (token.equals("+")) {
-                int a = result.pop();
-                int b = result.pop();
-                result.push(b + a);
-            } else if (token.equals("-")) {
-                int a = result.pop();
-                int b = result.pop();
-                result.push(b - a);
-            } else if (token.equals("*")) {
-                int a = result.pop();
-                int b = result.pop();
-                result.push(b * a);
-            } else if (token.equals("/")) {
-                int a = result.pop();
-                int b = result.pop();
-                result.push(b / a);
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (String token : tokens) {
+
+            if (token.equals("+") || token.equals("-") ||
+                    token.equals("*") || token.equals("/")) {
+
+                int b = stack.pop();
+                int a = stack.pop();
+
+                switch (token) {
+                    case "+": stack.push(a + b); break;
+                    case "-": stack.push(a - b); break;
+                    case "*": stack.push(a * b); break;
+                    case "/": stack.push(a / b); break;
+                }
+
             } else {
-                result.push(Integer.parseInt(token));
+                stack.push(Integer.parseInt(token));
             }
         }
 
-        return result.pop();
+        return stack.pop();
 
     }
 
