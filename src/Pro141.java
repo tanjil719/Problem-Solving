9f1cd715402e84529f715f23b4711e6d1a15055d
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-06-30 14:36:19 +0600

    Solved pro 141

---
+++ src/Pro141.java
@@ -0,0 +1,33 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+
+public class Pro141 {
+    public boolean hasCycle(ListNode head) {
+        if (head == null ){
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != slow){
+            if (fast == null || fast.next == null){
+                return false;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+
+    }
+}
