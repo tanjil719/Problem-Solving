becfbaa5a4075971e2a9b023eec2df6fc6e927e9
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-07-18 20:18:58 +0600

    Solved pro 2 by recursive approach

--- src/Pro2.java
+++ src/Pro2.java
@@ -1,26 +1,52 @@
 
 public class Pro2 {
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        ListNode dummy = new ListNode(0);
+//        ListNode current = dummy;
+//        int carry = 0;
+//
+//        while (l1 != null || l2 != null || carry != 0) {
+//            int x = (l1 != null) ? l1.val : 0;
+//            int y = (l2 != null) ? l2.val : 0;
+//            int total = x + y + carry;
+//
+//            carry = total / 10;
+//            int newValue = total % 10;
+//
+//            current.next = new ListNode(newValue);
+//            current = current.next;
+//
+//            if (l1 != null) l1 = l1.next;
+//            if (l2 != null) l2 = l2.next;
+//        }
+//
+//        return dummy.next;
+//    }
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-        int carry = 0;
+        return  this.add(l1, l2, 0);
+    }
 
-        while (l1 != null || l2 != null || carry != 0) {
-            int x = (l1 != null) ? l1.val : 0;
-            int y = (l2 != null) ? l2.val : 0;
-            int total = x + y + carry;
+    private ListNode add(ListNode l1, ListNode l2, int carry) {
 
-            carry = total / 10;
-            int newValue = total % 10;
-
-            current.next = new ListNode(newValue);
-            current = current.next;
-
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
+        if (l1 == null && l2 == null &&  carry == 0) {
+            return null;
         }
 
-        return dummy.next;
+        int sum = carry;
+        if (l1 != null) {
+            sum += l1.val;
+        }
+
+        if (l2 != null) {
+            sum += l2.val;
+        }
+
+        ListNode resultNode = new ListNode(sum % 10);
+        resultNode.next = add(l1 != null ? l1.next : null, l2 != null ? l2.next : null, sum / 10);
+
+        return resultNode;
     }
+
 
 }
