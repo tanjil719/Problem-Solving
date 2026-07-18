bae8e4c8fba551eef88ee12498bb33a9b81efe6b
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-07-18 21:14:13 +0600

    Solved pro 21 using recursive approach

--- src/Pro21.java
+++ src/Pro21.java
@@ -1,39 +1,59 @@
 public class Pro21 {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//
+//        if (list1 == null) {
+//            return list2;
+//        }
+//        if (list2 == null) {
+//            return list1;
+//        }
+//
+//        ListNode result = new ListNode(0);
+//        ListNode current = result;
+//
+//        while (list1 != null || list2 != null) {
+//
+//            if (list1 == null) {
+//                current.next = new ListNode(list2.val);
+//                current = current.next;
+//                list2 = list2.next;
+//            }else if (list2 == null) {
+//                current.next = new ListNode(list1.val);
+//                current = current.next;
+//                list1 = list1.next;
+//            }else {
+//                if (list1.val < list2.val) {
+//                    current.next = new ListNode(list1.val);
+//                    current = current.next;
+//                    list1 = list1.next;
+//                } else {
+//                    current.next = new ListNode(list2.val);
+//                    current = current.next;
+//                    list2 = list2.next;
+//                }
+//            }
+//        }
+//
+//        return result.next;
+//    }
 
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if (list1 == null) {
             return list2;
         }
+
         if (list2 == null) {
             return list1;
         }
 
-        ListNode result = new ListNode(0);
-        ListNode current = result;
+        if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
 
-        while (list1 != null || list2 != null) {
-
-            if (list1 == null) {
-                current.next = new ListNode(list2.val);
-                current = current.next;
-                list2 = list2.next;
-            }else if (list2 == null) {
-                current.next = new ListNode(list1.val);
-                current = current.next;
-                list1 = list1.next;
-            }else {
-                if (list1.val < list2.val) {
-                    current.next = new ListNode(list1.val);
-                    current = current.next;
-                    list1 = list1.next;
-                } else {
-                    current.next = new ListNode(list2.val);
-                    current = current.next;
-                    list2 = list2.next;
-                }
-            }
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
-
-        return result.next;
     }
+
 }
