7c8c133ac683af5fca40fdad50944cc067f94bc6
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-07-24 03:11:24 +0600

    Solved pro 19 using two pointer approach

--- src/Pro19.java
+++ src/Pro19.java
@@ -3,41 +3,64 @@ import java.util.HashMap;
 public class Pro19 {
     public ListNode removeNthFromEnd(ListNode head, int n) {
 
-        if (head == null || n <= 0) {
-            return head;
+//        if (head == null || n <= 0) {
+//            return head;
+//        }
+//
+//        HashMap<Integer, ListNode> map = new HashMap<>();
+//        ListNode current = head;
+//        int position = 0;
+//
+//        while (current != null) {
+//            map.put(++position, current);
+//            current = current.next;
+//        }
+//
+//        int fromLastPosition = position - n + 1;
+//
+//        ListNode firstNode = null;
+//        ListNode lastNode = null;
+//        ListNode deleteNode = map.get(fromLastPosition);
+//
+//        if (fromLastPosition > 1) {
+//            firstNode = map.get(fromLastPosition - 1);
+//        }else {
+//            return deleteNode.next;
+//        }
+//
+//        if(deleteNode.next != null) {
+//            lastNode = map.get(fromLastPosition+ 1);
+//        }else {
+//            firstNode.next = null;
+//            return head;
+//        }
+//
+//        firstNode.next = lastNode;
+//
+//        return head;
+
+        // Two pointer approach
+
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
         }
 
-        HashMap<Integer, ListNode> map = new HashMap<>();
-        ListNode current = head;
-        int position = 0;
-
-        while (current != null) {
-            map.put(++position, current);
-            current = current.next;
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
         }
 
-        int fromLastPosition = position - n + 1;
+        slow.next = slow.next.next;
 
-        ListNode firstNode = null;
-        ListNode lastNode = null;
-        ListNode deleteNode = map.get(fromLastPosition);
+        return dummy.next;
 
-        if (fromLastPosition > 1) {
-            firstNode = map.get(fromLastPosition - 1);
-        }else {
-            return deleteNode.next;
-        }
-
-        if(deleteNode.next != null) {
-            lastNode = map.get(fromLastPosition+ 1);
-        }else {
-            firstNode.next = null;
-            return head;
-        }
-
-        firstNode.next = lastNode;
-
-        return head;
     }
 
 }
