a3e8dca315db83d3a6f7a7564ffc3b725ad5a257
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-07-24 02:41:20 +0600

    Solved pro 19

---
+++ src/Pro19.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+
+public class Pro19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        if (head == null || n <= 0) {
+            return head;
+        }
+
+        HashMap<Integer, ListNode> map = new HashMap<>();
+        ListNode current = head;
+        int position = 0;
+
+        while (current != null) {
+            map.put(++position, current);
+            current = current.next;
+        }
+
+        int fromLastPosition = position - n + 1;
+
+        ListNode firstNode = null;
+        ListNode lastNode = null;
+        ListNode deleteNode = map.get(fromLastPosition);
+
+        if (fromLastPosition > 1) {
+            firstNode = map.get(fromLastPosition - 1);
+        }else {
+            return deleteNode.next;
+        }
+
+        if(deleteNode.next != null) {
+            lastNode = map.get(fromLastPosition+ 1);
+        }else {
+            firstNode.next = null;
+            return head;
+        }
+
+        firstNode.next = lastNode;
+
+        return head;
+    }
+
+}
