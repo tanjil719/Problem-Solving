9b783c1dcf715d5d3b8515c7208f9f9db9040e99
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-07-25 13:24:29 +0600

    code reformatted

--- src/Pro3514.java
+++ src/Pro3514.java
@@ -1,32 +1,32 @@
 import java.util.Arrays;
 
 public class Pro3514 {
-    public static int uniqueXorTriplets(int[] arr) {
-        int n = arr.length;
+    public static int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
         boolean[] freq = new boolean[2048];
         int len = 0, idx = 0, ans = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-                if (!freq[arr[i] ^ arr[j]]) {
+                if (!freq[nums[i] ^ nums[j]]) {
                     len++;
-                    freq[arr[i] ^ arr[j]] = true;
+                    freq[nums[i] ^ nums[j]] = true;
                 }
             }
         }
 
-        int[] nums = new int[len];
+        int[] ar = new int[len];
 
         for (int i = 0; i < 2048; i++) {
             if (freq[i]) {
-                nums[idx++] = i;
+                ar[idx++] = i;
             }
         }
         Arrays.fill(freq, false);
 
         for (int i = 0; i < len; i++) {
             for (int j = 0; j < n; j++) {
-                freq[nums[i] ^ arr[j]] = true;
+                freq[ar[i] ^ nums[j]] = true;
             }
         }
 
