8d849a72879f9a8ddf7b7418b1b5ff81ee115d85
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-07-27 00:43:48 +0600

    Pro 61 solved

---
+++ src/Pro61.java
@@ -0,0 +1,35 @@
+public class Pro61 {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        int length = 1;
+        ListNode tail = head;
+
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+
+        k = k % length;
+
+        if (k == 0) {
+            return head;
+        }
+
+        tail.next = head;
+
+        int newHeadPosition = length - k;
+
+        ListNode newTail = head;
+        for (int i = 1; i < newHeadPosition; i++) {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+        return newHead;
+
+    }
+}
